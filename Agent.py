7092c3973e59b421d79df50a823e6ff0e280c58f
JenniferEA <JennyAgar@web.de>
2020-06-12 10:54:09 +0200

    DQN Replay Memory, Epsilon Greedy, Main Loop

--- Agent.py
+++ Agent.py
@@ -1,11 +1,25 @@
 import random
+import numpy as np
 import tensorflow as tf
 import keras as k
 
 
 class Agent:
-    def __init__(self):
+    def __init__(self, strategy):
         self.total_reward = 0.0
+        self.current_step = 0
+        self.strategy = strategy
 
-    def predict(self, obs, possible_actions):
-        return random.choice(possible_actions), random.randint(0, 10)          # wählt zufällige Aktion
+    def predict(self, state, possible_actions, policy_net):
+        rate = self.strategy.get_exploration_rate(self.current_step)
+        self.current_step += 1
+
+        print(rate)
+        # Exploration Exploitation Trade Off
+        if rate > random.random():
+            return random.choice(possible_actions)   # explore
+        else:
+            return np.argmax(policy_net(state)[0])     # exploit
+
+
+        # return random.choice(possible_actions), random.randint(0, 10)          # wählt zufällige Aktion
