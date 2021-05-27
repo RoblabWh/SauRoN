77e88aeacb08a7408d2e976a620ce5c49ebdbbf7
SchulteM42 <MAx.Schulte_1@studmail.w-hs.de>
2021-05-27 16:08:23 +0200

    Korrektur der Dokumentation

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ num_episodes = 2500         # the number of epochs (/episodes) that are simulate
 steps = 600                 # number of steps per epoch (/episode)
 trainingInterval = 75       # number of steps after which the neural net is trained
 
-arenaWidth = 22             # in meter
-arenaLength = 10            # in meter
+arenaWidth = 22             # Width (X Direction) of the Arena in Meter
+arenaLength = 10            # Length (Y direction) of the Arena in meter
 simTimeStep = 0.15          # simulated time between two steps in the simulation
 
 angleStepsSonar = 0.5       # spacing between two light rays (for distance calculation) in degrees
