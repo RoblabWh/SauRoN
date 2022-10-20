6e24d9c36b5be2b464ad2c76d3e20238213b9e4e
Niklas Digakis <niklas.digakis@studmail.w-hs.de>
2022-10-20 21:06:15 +0200

    add requirements

---
+++ install_conda_env.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+default_env_name="sauron"
+
+read -p "Name for virtual environment (default: ${default_env_name}): " env_name
+
+if [ -z ${env_name} ]; then
+  env_name=${default_env_name}
+fi
+
+conda create --name ${env_name} --file requirements.txt --channel default --channel pytorch --channel conda-forge
