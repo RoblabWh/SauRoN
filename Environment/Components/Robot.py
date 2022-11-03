fixed pytorch not using multiple processes, fixed calculation of velocitys, fixed tensorboard

Environment/Components/Robot.py
@@ -70,8 +70,8 @@ class Robot:
         self.minAngularAcceleration = -1.5 * math.pi  #rad/s^2
 
         #Factors for normalization
-        self.maxLinearVelocityFact = 1/self.maxLinearVelocity
-        self.maxAngularVelocityFact = 1/self.maxAngularVelocity
+        #self.maxLinearVelocityFact = 1/self.maxLinearVelocity
+        #self.maxAngularVelocityFact = 1/self.maxAngularVelocity
         # Maximum distance in laserscan is 20 meters
         self.maxDistFact = 1/20
 
@@ -255,6 +255,7 @@ class Robot:
             linVel = self.linTast
             angVel = self.angTast
 
+        #print("linVel: ", linVel, "angVel: ", angVel)
         oldDir = self.getDirectionAngle()
         directionVector = self.directionVectorFromAngle(oldDir)
 
@@ -270,6 +271,8 @@ class Robot:
         goalDist = math.sqrt((posX-goalX)**2+(posY-goalY)**2)
 
         frame = [posX, posY, directionVector[0], directionVector[1], linVel, angVel, goalX, goalY, goalDist, direction]
+        #print("linVel: ", linVel, "angVel: ", angVel)
+        #print("linVelNorm: ", self.getLinearVelocityNorm(), "angVelNorm: ", self.getAngularVelocityNorm())
         self.push_frame(frame)
         if self.hasPieSlice:
             self.updatePieSlice(deltaDir, (deltaPosX, deltaPosY))
@@ -470,45 +473,60 @@ class Robot:
         :return: tuple
             (float - linear velocity, float - angular velocity)
         """
-        self.netOutput = (tarAngVel, tarLinVel)
+        self.netOutput = (tarLinVel, tarAngVel)
 
-        if(tarLinVel > 1 or tarAngVel > 1 or tarLinVel < -1 or tarLinVel <- 1):
-            print("velocitiy recieved from neural net is out of  bounds. Fix your Code!  ", tarLinVel, tarAngVel)
-        tarLinVel = max(-1, min(tarLinVel, 1))
-        tarAngVel = max(-1, min(tarAngVel, 1))
+        assert(tarLinVel <= 1 or tarAngVel <= 1 or tarLinVel >= -1 or tarAngVel >= -1), f"velocitiy recieved from neural net is out of  bounds. Fix your Code! {tarLinVel}, {tarAngVel}"
+        # tarLinVel = max(-1, min(tarLinVel, 1))
+        # tarAngVel = max(-1, min(tarAngVel, 1))
 
+        #print("linVel: ", linVel, "angVel: ", angVel)
+        #print("tarLinVelNorm: ", tarLinVel, "tarAngVelNorm: ", tarAngVel)
         # mapping the net output range of -1 to 1 onto the velocitiy ranges of the robot
         # tarAngVel = tarAngVel * ((self.maxAngularVelocity - self.minAngularVelocity)* 0.5) + (self.maxAngularVelocity + self.minAngularVelocity) * 0.5
-        tarAngVel = tarAngVel * ((self.maxAngularVelocity - self.minAngularVelocity)* 0.5) + ((self.minAngularVelocity + self.maxAngularVelocity) * 0.5)
+        tarAngVel = tarAngVel * ((self.maxAngularVelocity - self.minAngularVelocity) * 0.5) + ((self.minAngularVelocity + self.maxAngularVelocity) * 0.5)
         tarLinVel = tarLinVel * ((self.maxLinearVelocity - self.minLinearVelocity) * 0.5) + ((self.minLinearVelocity + self.maxLinearVelocity) * 0.5)
+        # # beschleunigen
+        # if linVel < tarLinVel:
+        #     linVel += self.maxLinearAcceleration * dt  # v(t) = v(t-1) + a * dt
+        #     if linVel > self.maxLinearVelocity:
+        #         linVel = self.maxLinearVelocity
+        #
+        # # bremsen
+        # elif linVel > tarLinVel:
+        #     linVel += self.minLinearAcceleration * dt
+        #     if linVel < self.minLinearVelocity:
+        #         linVel = self.minLinearVelocity
+        #
+        # # nach links drehen
+        # if angVel < tarAngVel:
+        #     angVel += self.maxAngularAcceleration * dt
+        #     if angVel > self.maxAngularVelocity:
+        #         angVel = self.maxAngularVelocity
+        #
+        # # nach rechts drehen
+        # elif angVel > tarAngVel:
+        #     angVel += self.minAngularAcceleration * dt
+        #     if angVel < self.minAngularVelocity:
+        #         angVel = self.minAngularVelocity
 
-        # beschleunigen
-        if linVel < tarLinVel:
-            linVel += self.maxLinearAcceleration * dt  # v(t) = v(t-1) + a * dt
-            if linVel > self.maxLinearVelocity:
-                linVel = self.maxLinearVelocity
-
-        # bremsen
-        elif linVel > tarLinVel:
-            linVel += self.minLinearAcceleration * dt
-            if linVel < self.minLinearVelocity:
-                linVel = self.minLinearVelocity
-
-        # nach links drehen
-        if angVel < tarAngVel:
-            angVel += self.maxAngularAcceleration * dt
-            if angVel > self.maxAngularVelocity:
-                angVel = self.maxAngularVelocity
-
-        # nach rechts drehen
-        elif angVel > tarAngVel:
-            angVel += self.minAngularAcceleration * dt
-            if angVel < self.minAngularVelocity:
-                angVel = self.minAngularVelocity
-
+        # return linVel, angVel
         # maybe ändern
-        #return tarLinVel, tarAngVel
-        return linVel, angVel
+
+        # check boundaries
+        if tarLinVel > self.maxLinearVelocity:
+            tarLinVel = self.maxLinearVelocity
+
+        if tarLinVel < self.minLinearVelocity:
+            tarLinVel = self.minLinearVelocity
+
+        if tarAngVel > self.maxAngularVelocity:
+            tarAngVel = self.maxAngularVelocity
+
+        if tarAngVel < self.minAngularVelocity:
+            tarAngVel = self.minAngularVelocity
+
+        return np.around(tarLinVel, decimals=5), np.around(tarAngVel, decimals=5)
+
 
     def directionVectorFromAngle(self, direction):
         """
@@ -527,7 +545,7 @@ class Robot:
         station = self.station
         distance2StationCenter = math.sqrt((station.getPosX() - self.getPosX())**2 +
                                            (station.getPosY() - self.getPosY())**2) + self.radius
-        return (distance2StationCenter < station.radius)
+        return (distance2StationCenter < (station.radius * 1.2))
 
     def collideWithTargetStationRectengular(self):
         """
@@ -578,18 +596,18 @@ class Robot:
         return self.state_raw[self.time_steps - 2][3]
 
     def getLinearVelocity(self):
-        return self.state_raw[self.time_steps - 1][4]
+        return np.around(self.state_raw[self.time_steps - 1][4], decimals=5)
 
     def getAngularVelocity(self):
-        return self.state_raw[self.time_steps - 1][5]
+        return np.around(self.state_raw[self.time_steps - 1][5], decimals=5)
 
     def getLinearVelocityNorm(self):
-        return (self.getLinearVelocity() - ((self.minLinearVelocity + self.maxLinearVelocity) * 0.5)) / (
-                    (self.maxLinearVelocity - self.minLinearVelocity) * 0.5)
+        return np.around((self.getLinearVelocity() - ((self.minLinearVelocity + self.maxLinearVelocity) * 0.5)) / (
+                    (self.maxLinearVelocity - self.minLinearVelocity) * 0.5), decimals=5)
 
     def getAngularVelocityNorm(self):
-        return (self.getAngularVelocity() - ((self.minAngularVelocity + self.maxAngularVelocity) * 0.5)) / (
-                (self.maxAngularVelocity - self.minAngularVelocity) * 0.5)
+        return np.around((self.getAngularVelocity() - ((self.minAngularVelocity + self.maxAngularVelocity) * 0.5)) / (
+                (self.maxAngularVelocity - self.minAngularVelocity) * 0.5), decimals=5)
 
     def getGoalX(self):
         return self.state_raw[self.time_steps - 1][6]
@@ -628,13 +646,13 @@ class Robot:
 
     def on_press(self, key):
         if key.char == 'w':
-            self.linTast = 1.5
+            self.linTast = 1.49999
         if key.char == 'a':
-            self.angTast = -1.6
+            self.angTast = -0.5999
         if key.char == 's':
-            self.linTast = -1.5
+            self.linTast = 0
         if key.char == 'd':
-            self.angTast = 1.6
+            self.angTast = 0.5 #0.5999
         if key.char == 'c':
             self.angTast = 0
 

Environment/Environment.py
@@ -160,47 +160,64 @@ class Environment:
         """
 
         reward = 0
-        r_arrival = 70
-        r_collision = -20
-        r_stop = -0.5
-        w_gp = 2.5
-        w_gn = 1.2
-        w_d = 2.0
+        r_arrival = 20 # reward for reaching the goal
+        r_collision = -5 # Robot crashed with a wall or another robot
+        r_runOutOfTime = 0 # Robot has run out of time
+        r_stop = -0.2 # Robot stood still
+        w_gp = 0.01
+        w_gn = 0.005
+        w_d = 0.2 # weight for the distance
         w_w = -0.001
-        a_p = 0.3
+        a_p = 0.002 # weight for the angle, always positive
 
         if reachedPickup:
             reward = r_arrival
         elif runOutOfTime:
-            reward = 0
+            reward = r_runOutOfTime
         elif collision:
             reward = r_collision
         else:
+
+            if dist_old > dist_new:
+                reward += w_gp * (dist_old - dist_new)
+            else:
+                reward += w_gn * (dist_old - dist_new)
+
+            #TODO compute velocity discount factor (reward robot for moving slower linear)
+            # linvel = robot.getLinearVelocity()
+            # dist = (dist_old - dist_new)
+            # vel_discount = np.power((1.5 - max(linvel, 0.1)), (1 / max(dist, 0.1)))
+            # print("reward before: ", reward)
+            # print("linvel:", linvel, "veldiscount:", vel_discount, "dist:", (dist_old - dist_new))
+            # reward = vel_discount * (dist_old - dist_new)
             # if dist_old > dist_new:
-            #     reward += w_gp * (dist_old - dist_new)
+            #     reward *= w_gp
             # else:
-            #     reward += w_gn * (dist_old - dist_new)
-            if dist_old == dist_new:
-                reward += r_stop
-            if dist_new < robot.initialGoalDist:
-                reward += w_d * (robot.initialGoalDist - dist_new)
-                robot.initialGoalDist = dist_new
+            #     reward *= w_gn
+            #print("reward after: ", reward)
 
-            #a1 = np.arctan2(robot.getGoalY() - robot.getPosY(), robot.getGoalX() - robot.getPosX())
-            #a2 = np.arctan2(robot.getDirectionY(), robot.getDirectionX())
-            #goalangle = np.abs(a1 - a2)
-
-            goalangle = np.arctan2(robot.debugAngle[1], robot.debugAngle[0])
-            alpha_norm = 1 - (goalangle / np.pi)
-            if alpha_norm > 0:
+            # Directional reward (look at the angle between the robot and the goal)
+            a1 = np.arctan2(robot.getGoalY() - robot.getPosY(), robot.getGoalX() - robot.getPosX())
+            a2 = np.arctan2(robot.getDirectionY(), robot.getDirectionX())
+            goalangle = np.abs(a1 - a2)
+            if goalangle < np.pi/4:
+                alpha_norm = 1 - goalangle
                 reward += a_p * alpha_norm
 
+            #print(np.around(reward, decimals=5))
+
+            # PUBG Reward (only gets rewarded if it gets closer to the goal than previously)
+            # if dist_old == dist_new:
+            #     reward += r_stop
+            # if dist_new < robot.initialGoalDist:
+            #     reward += w_d * (robot.initialGoalDist - dist_new)
+            #     robot.initialGoalDist = dist_new
 
             # abs_ang_vel = np.abs(robot.getAngularVelocity())
             # if abs_ang_vel > 0.7:
             #     reward += w_w * abs_ang_vel
 
-        return reward
+        return np.around(reward, decimals=5)
 
     def reset(self, level=None):
         """

PPO/PPOAlgorithm.py
@@ -80,10 +80,8 @@ class Actor(nn.Module):
         mu = torch.tanh(self.mu(x))
         std = torch.exp(self.log_std)
         var = torch.pow(std, 2)
-        cov_mat = torch.diag(var)
-        dist = MultivariateNormal(mu, cov_mat)
 
-        return mu, var, dist
+        return mu.to('cpu'), var.to('cpu')
 
 
 class Critic(nn.Module):
@@ -119,10 +117,12 @@ class ActorCritic(nn.Module):
         laser, orientation, distance, velocity = states
         # TODO: check if normalization of states is necessary
         # was suggested in: Implementation_Matters in Deep RL: A Case Study on PPO and TRPO
-        action_mean, action_var, dist = self.actor(laser, orientation, distance, velocity)
+        action_mean, action_var = self.actor(laser, orientation, distance, velocity)
 
-        ## logging of actions TODO: log in SingleEnvironment
-        #self.logger.add_actor_output(action_mean.mean(0)[0].item(), action_mean.mean(0)[1].item(), action_var[0].item(), action_var[1].item())
+        cov_mat = torch.diag(action_var)
+        dist = MultivariateNormal(action_mean, cov_mat)
+        ## logging of actions
+        self.logger.add_actor_output(action_mean.mean(0)[0].item(), action_mean.mean(0)[1].item(), action_var[0].item(), action_var[1].item())
 
         action = dist.sample()
         action = torch.clip(action, -1, 1)
@@ -136,7 +136,7 @@ class ActorCritic(nn.Module):
 
     def act_certain(self, states, memory):
         laser, orientation, distance, velocity = states
-        action, _, _ = self.actor(laser, orientation, distance, velocity)
+        action, _ = self.actor(laser, orientation, distance, velocity)
 
         memory.insertState(laser.to(device).detach(), orientation.to(device).detach(), distance.to(device).detach(), velocity.to(device).detach())
         memory.insertAction(action)
@@ -147,8 +147,10 @@ class ActorCritic(nn.Module):
         laser, orientation, distance, velocity = state
         state_value = self.critic(laser, orientation, distance, velocity)
 
-        _, _, dist = self.actor(laser, orientation, distance, velocity)
+        action_mean, action_var = self.actor(laser, orientation, distance, velocity)
 
+        cov_mat = torch.diag(action_var.to(device))
+        dist = MultivariateNormal(action_mean.to(device), cov_mat)
         action_logprobs = dist.log_prob(action)
         dist_entropy = dist.entropy()
 

utils.py
@@ -217,21 +217,16 @@ class Logger(object):
         self.actor_var_linvel.append(actor_var_linvel)
         self.actor_var_angvel.append(actor_var_angvel)
 
-    def summary_actor_output(self):
+    def summary_actor_output(self, actor_mean_linvel, actor_mean_angvel, actor_var_linvel, actor_var_angvel):
         if self.logging:
-            self.writer.add_scalars('actor_output', {'Mean LinVel': np.mean(self.actor_mean_linvel),
-                                                     'Mean AngVel': np.mean(self.actor_mean_angvel),
-                                                     'Variance LinVel': np.mean(self.actor_var_linvel),
-                                                     'Variance AngVel': np.mean(self.actor_var_angvel)}, self.episode)
-        self.actor_mean_linvel = []
-        self.actor_mean_angvel = []
-        self.actor_var_linvel = []
-        self.actor_var_angvel = []
+            self.writer.add_scalars('actor_output', {'Mean LinVel': actor_mean_linvel,
+                                                     'Mean AngVel': actor_mean_angvel,
+                                                     'Variance LinVel': actor_var_linvel,
+                                                     'Variance AngVel': actor_var_angvel}, self.episode)
 
-    def summary_objective(self):
+    def summary_objective(self, objective_reached):
         if self.logging:
-            self.writer.add_scalar('percentage_objective_reached', self.percentage_objective_reached(), self.episode)
-        self.objective_reached = 0
+            self.writer.add_scalar('percentage_objective_reached', objective_reached, self.episode)
 
     def add_reward(self, reward):
         self.scalar_summary('reward', reward)
@@ -251,6 +246,13 @@ class Logger(object):
 
     def set_number_of_agents(self, number_of_agents):
         self.number_of_agents = number_of_agents
+
+    def clear_summary(self):
+        self.actor_mean_linvel = []
+        self.actor_mean_angvel = []
+        self.actor_var_linvel = []
+        self.actor_var_angvel = []
+        self.objective_reached = 0
 
     def close(self):
         self.writer.close()